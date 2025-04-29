c18b6d18a0f9dd75f6512202ed88a0256f902773
Cole Lowery <CLowery@student.neumont.edu>
2025-04-29 14:02:34 -0600

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,1 +1,52 @@
+#include "raylib.h"
+#include "Game.h"
 
+#if defined(PLATFORM_WEB)
+#include <emscripten/emscripten.h>
+#endif
+
+Game game;
+
+void UpdateDrawFrame() {
+    game.Update();
+    game.Draw();
+}
+
+int main() {
+    InitWindow(1600, 900, "Snake");
+    game.Init();
+
+#if defined(PLATFORM_WEB)
+    emscripten_set_main_loop(UpdateDrawFrame, 60, 1);
+#else
+    SetTargetFPS(40);
+    while (!WindowShouldClose()) {
+        UpdateDrawFrame();
+    }
+#endif
+
+    game.Unload();
+    CloseWindow();
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
